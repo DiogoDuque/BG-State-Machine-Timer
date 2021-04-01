b3d579bc72939f8ed78a194836a8a6604ce01065
DuqueD <diogo.serra.duque@paddypowerbetfair.com>
2021-04-01 15:27:56 +0100

    enable choice among multiple state machines

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
 import GoTStateMachine from './GoTStateMachine';
 import SpiritIslandStateMachine from './SpiritIslandStateMachine';
 
-class App extends Component {
+const allStateMachines = {
+  'GoT - Board Game': <GoTStateMachine />,
+  'Spirit Island': <SpiritIslandStateMachine />,
+};
 
-  // setup states
-  componentDidMount() {
-    
-  }
-
-  render() {
-    return (
-      <div>
-        <SpiritIslandStateMachine/>
-      </div>
-    );
-  }
+const App = () => {
+  const [chosenStateMachine, setChosenStateMachine] = useState();
+  return (
+    <div>
+      {!chosenStateMachine
+        ? (<MenuList>
+            {Object.entries(allStateMachines).map(e => <MenuItem key={e[0]} onClick={() => setChosenStateMachine(e[1])}>{e[0]}</MenuItem>)}
+          </MenuList>)
+        : chosenStateMachine
+      }
+    </div>
+  );
 }
+
 
 export default App;
